a7015ef4661166abe5497a62b91bd8b065121b05
Rafael Moura <rafael.mesquita.moura@gmail.com>
2024-05-01 16:56:38 -0300

    First Commit

---
+++ src/test/java/br/com/fiap/techchallenge/TechChallengeGrupo10ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.techchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechChallengeGrupo10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
