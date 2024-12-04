4d3fb683a2e12c89864d92b4b47144681adeb278
alvesoliveirasolucoes <alvesoliveirasoluces@gmail.com>
2024-12-03 21:43:54 -0300

    update pom.xml

--- src/test/java/br/com/fiap/techchallenge/infra/gateways/produto/ProdutoRepositoryTest.java
+++ src/test/java/br/com/fiap/techchallenge/infra/gateways/produto/ProdutoRepositoryTest.java
@@ -11,12 +11,15 @@ import br.com.fiap.techchallenge.infra.persistence.ProdutoEntityRepository;
 import br.com.fiap.techchallenge.infra.persistence.entities.CategoriaEntity;
 import br.com.fiap.techchallenge.infra.persistence.entities.ProdutoEntity;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.fge.jsonpatch.JsonPatch;
+import com.github.fge.jsonpatch.JsonPatchException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import java.math.BigDecimal;
@@ -65,14 +68,9 @@ class ProdutoRepositoryTest {
 
     @Test
     void listarProdutosTest() {
-        // Arrange
         when(produtoEntityRepository.findAll()).thenReturn(Collections.singletonList(produtoEntity));
         when(produtoMapper.fromListEntityToListDomain(anyList())).thenReturn(Collections.singletonList(produto));
-
-        // Act
         List<Produto> produtos = produtoRepository.listarProdutos(null, null, null);
-
-        // Assert
         assertNotNull(produtos);
         assertEquals(1, produtos.size());
         assertEquals(produto.getNome(), produtos.get(0).getNome());
@@ -84,11 +82,7 @@ class ProdutoRepositoryTest {
         // Arrange
         when(produtoEntityRepository.findAllByCategoriaEntityId(anyLong())).thenReturn(Optional.of(Collections.singletonList(produtoEntity)));
         when(produtoMapper.fromListEntityToListDomain(anyList())).thenReturn(Collections.singletonList(produto));
-
-        // Act
         List<Produto> produtos = produtoRepository.listarProdutosPorCategoria(CategoriaEnum.LANCHE);
-
-        // Assert
         assertNotNull(produtos);
         assertEquals(1, produtos.size());
         assertEquals(produto.getNome(), produtos.get(0).getNome());
@@ -97,17 +91,12 @@ class ProdutoRepositoryTest {
 
     @Test
     void criarProdutoTest() {
-        // Arrange
         when(categoriaEntityRepository.findByNome(anyString())).thenReturn(Optional.of(categoriaEntity));
         when(produtoEntityRepository.findByNome(anyString())).thenReturn(Optional.empty());
         when(produtoMapper.fromDomainToEntity(any(Produto.class))).thenReturn(produtoEntity);
         when(produtoEntityRepository.saveAndFlush(any(ProdutoEntity.class))).thenReturn(produtoEntity);
         when(produtoMapper.fromEntityToDomain(any(ProdutoEntity.class))).thenReturn(produto);
-
-        // Act
         Produto createdProduto = produtoRepository.criarProduto(produto);
-
-        // Assert
         assertNotNull(createdProduto);
         assertEquals(produto.getNome(), createdProduto.getNome());
         verify(produtoEntityRepository, times(1)).saveAndFlush(any(ProdutoEntity.class));
@@ -115,19 +104,13 @@ class ProdutoRepositoryTest {
 
     @Test
     void deletarProdutoTest() {
-        // Arrange
         when(produtoEntityRepository.findById(anyLong())).thenReturn(Optional.of(produtoEntity));
-
-        // Act
         produtoRepository.deletarProduto(1L);
-
-        // Assert
         verify(produtoEntityRepository, times(1)).deleteById(anyLong());
     }
 
     @Test
     void atualizarProdutoTest() {
-        // Arrange
         ProdutoEntity updatedProdutoEntity = new ProdutoEntity();
         updatedProdutoEntity.setNome("Updated Produto");
         updatedProdutoEntity.setDescricao("Updated Descrição");
@@ -140,11 +123,7 @@ class ProdutoRepositoryTest {
         when(produtoEntityRepository.saveAndFlush(any(ProdutoEntity.class))).thenReturn(updatedProdutoEntity);
 
         when(produtoMapper.fromEntityToDomain(updatedProdutoEntity)).thenReturn(produto);
-
-        // Act
         Produto updatedProduto = produtoRepository.atualizarProduto(1L, produto);
-
-        // Assert
         assertNotNull(updatedProduto);
         assertEquals("Produto Teste", updatedProduto.getNome());
         verify(produtoEntityRepository, times(1)).saveAndFlush(any(ProdutoEntity.class));
@@ -152,7 +131,6 @@ class ProdutoRepositoryTest {
 
     @Test
     void atualizarDadosProdutoTest() throws JsonProcessingException {
-        // Arrange
         ObjectMapper objectMapper = new ObjectMapper();
         String patchJson = "[ {\"op\": \"replace\", \"path\": \"/nome\", \"value\": \"Produto 1\"} ]";
         JsonPatch validPatch = objectMapper.readValue(patchJson, JsonPatch.class);
@@ -167,11 +145,7 @@ class ProdutoRepositoryTest {
         when(produtoEntityRepository.findById(anyLong())).thenReturn(Optional.of(produtoEntity));
         when(produtoEntityRepository.saveAndFlush(any(ProdutoEntity.class))).thenReturn(updatedProdutoEntity);
         when(produtoMapper.fromEntityToDomain(any(ProdutoEntity.class))).thenReturn(produtoUpdate);
-
-        // Act
         Produto updatedProduto = produtoRepository.atualizarDadosProduto(1L, validPatch);
-
-        // Assert
         assertNotNull(updatedProduto);
         assertEquals("Updated Produto", updatedProduto.getNome());
         assertEquals(BigDecimal.valueOf(200), updatedProduto.getPreco());
@@ -183,11 +157,7 @@ class ProdutoRepositoryTest {
         // Arrange
         when(produtoEntityRepository.findById(anyLong())).thenReturn(Optional.of(produtoEntity));
         when(produtoMapper.fromEntityToDomain(any(ProdutoEntity.class))).thenReturn(produto);
-
-        // Act
         Produto foundProduto = produtoRepository.findById(1L);
-
-        // Assert
         assertNotNull(foundProduto);
         assertEquals(produto.getNome(), foundProduto.getNome());
         verify(produtoEntityRepository, times(1)).findById(anyLong());
@@ -195,10 +165,7 @@ class ProdutoRepositoryTest {
 
     @Test
     void findByIdProdutoNotFoundTest() {
-        // Arrange
         when(produtoEntityRepository.findById(anyLong())).thenReturn(Optional.empty());
-
-        // Act & Assert
         ProdutoException exception = assertThrows(ProdutoException.class, () -> {
             produtoRepository.findById(1L);
         });
@@ -207,13 +174,75 @@ class ProdutoRepositoryTest {
 
     @Test
     void listarProdutosPorCategoriaNaoEncontradaTest() {
-        // Arrange
         when(produtoEntityRepository.findAllByCategoriaEntityId(anyLong())).thenReturn(Optional.empty());
-
-        // Act & Assert
         ProdutoException exception = assertThrows(ProdutoException.class, () -> {
             produtoRepository.listarProdutosPorCategoria(CategoriaEnum.LANCHE);
         });
         assertEquals(ErrosEnum.PRODUTO_CATEGORIA_NAO_ENCONTRADO.getMessage(), exception.getMessage());
     }
+
+    @Test
+    void criarProdutoDeveraRetornarUmExcecaoDeQueOProdutoJaExiste() {
+        CategoriaEntity categoriaEntity = new CategoriaEntity();
+        categoriaEntity.setId(1L);
+        categoriaEntity.setNome(CategoriaEnum.LANCHE.name());
+
+        Produto produtoCriar = new Produto("Big Fiap", "Nova Descrição", new Categoria("LANCHE", "Lanches"), BigDecimal.valueOf(200), "imagem.jpg");
+
+        ProdutoEntity produtoEntity = new ProdutoEntity();
+        produtoEntity.setId(1L);
+        produtoEntity.setNome("Produto Teste");
+        produtoEntity.setDescricao("Descrição do produto");
+        produtoEntity.setPreco(BigDecimal.valueOf(100));
+        produtoEntity.setCategoriaEntity(categoriaEntity);
+
+        when(categoriaEntityRepository.findByNome("LANCHE")).thenReturn(Optional.of(categoriaEntity));
+        when(produtoEntityRepository.findByNome("Big Fiap")).thenReturn(Optional.of(produtoEntity));
+
+        ProdutoException thrown = assertThrows(ProdutoException.class, () -> {
+            produtoRepository.criarProduto(produtoCriar);
+        });
+
+        assertEquals(ErrosEnum.PRODUTO_JA_EXISTE.getMessage(), thrown.getError().getMessage());
+        verify(categoriaEntityRepository, times(1)).findByNome("LANCHE");
+        verify(produtoEntityRepository, times(1)).findByNome("Big Fiap");
+    }
+
+    @Test
+    void testaExcecaoAoDeletarUmProdutoComIdentificadorInvalido(){
+        when(produtoEntityRepository.findById(1l)).thenReturn(Optional.empty());
+        ProdutoException thrown = assertThrows(ProdutoException.class, () -> {
+            produtoRepository.deletarProduto(1l);
+        });
+
+        assertEquals(ErrosEnum.PRODUTO_CODIGO_IDENTIFICADOR_INVALIDO.getMessage(), thrown.getError().getMessage());
+        verify(produtoEntityRepository, times(1)).findById(1l);
+    }
+
+    @Test
+    void testaListarTodosOsProdutosComNomeDescricaoEPreco(){
+        String nome = "nome";
+        String descricao = "descricao";
+        BigDecimal preco = new BigDecimal("10");
+        Produto produtoCriar = new Produto("Big Fiap", "Nova Descrição", new Categoria("LANCHE", "Lanches"), BigDecimal.valueOf(200), "imagem.jpg");
+        List<ProdutoEntity> produtoEntityList = List.of(produtoEntity);
+        List<Produto> produtoDomain = List.of(produtoCriar);
+
+        when(produtoEntityRepository.findByNomeOrDescricaoOrPreco(nome,descricao,preco)).thenReturn(Optional.of(produtoEntityList));
+        when(produtoMapper.fromListEntityToListDomain(produtoEntityList)).thenReturn(produtoDomain);
+
+        produtoRepository.listarProdutos(nome, descricao, preco);
+        verify(produtoEntityRepository, times(1)).findByNomeOrDescricaoOrPreco(nome,descricao,preco);
+    }
+
+    @Test
+    void criarProdutoDeveraRetornarSeACategoriaNaoExists() {
+
+        when(categoriaEntityRepository.findByNome("LANCHE")).thenReturn(Optional.empty());
+        Produto produtoCriar = new Produto("Big Fiap", "Nova Descrição", new Categoria("LANCHE", "Lanches"), BigDecimal.valueOf(200), "imagem.jpg");
+
+        assertNull(produtoRepository.criarProduto(produtoCriar));
+        verify(categoriaEntityRepository, times(1)).findByNome("LANCHE");
+    }
+
 }
